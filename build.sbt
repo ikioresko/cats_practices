330cee1ff3d081561715954f21a77b012a4313e9
ikioresko <ikioresko@gmail.com>
2023-05-10 18:56:03 +0500

    tapir Swagger

--- build.sbt
+++ build.sbt
@@ -1,7 +1,7 @@
 name := "cats-practices"
 version := "2.5.3"
 scalaVersion := "2.12.8"
-
+val tapirVersion = "1.2.9"
 val http4sVersion = "0.23.9"
 val CirceVersion = "0.14.1"
 
@@ -20,5 +20,9 @@ libraryDependencies ++= Seq(
   "org.http4s" %% "http4s-circe" % http4sVersion,
   "org.http4s" %% "http4s-dropwizard-metrics" % http4sVersion,
   "io.circe" %% "circe-generic" % CirceVersion,
-  "io.circe" %% "circe-parser" % CirceVersion
+  "io.circe" %% "circe-parser" % CirceVersion,
+  "com.softwaremill.sttp.tapir" %% "tapir-http4s-server" % tapirVersion,
+  "com.softwaremill.sttp.tapir" %% "tapir-swagger-ui-bundle" % tapirVersion,
+  "com.softwaremill.sttp.tapir" %% "tapir-json-circe" % tapirVersion,
+  "ch.qos.logback" % "logback-classic" % "1.4.7",
 )
--- src/main/scala/cats_task/http4s_example/api/Api.scala
+++ src/main/scala/cats_task/http4s_example/api/Api.scala
@@ -1,16 +1,31 @@
 package cats_task.http4s_example.api
 
-import cats.data.Kleisli
+import cats_task.model.Library._
 import cats.effect.IO
-import cats_task.http4s_example.User
-import io.circe.syntax._
-import org.http4s.{HttpRoutes, Request, Response}
-import org.http4s.dsl.io._
-import org.http4s.implicits._
+import cats_task.model.{Library, User}
+import io.circe.generic.auto._
+import sttp.tapir._
+import sttp.tapir.generic.auto._
+import sttp.tapir.json.circe._
+import sttp.tapir.server.ServerEndpoint
+import sttp.tapir.swagger.bundle.SwaggerInterpreter
 
 object Api {
-  val helloService: Kleisli[IO, Request[IO], Response[IO]] = HttpRoutes.of[IO] {
-    case GET -> Root / name =>
-      Ok(s"Hello, ${User("yourLogin", name).asJson}.")
-  }.orNotFound
+  val helloEndpoint: PublicEndpoint[User, Unit, String, Any] = endpoint.get
+    .in("hello")
+    .in(query[User]("name"))
+    .out(stringBody)
+  val helloServerEndpoint: ServerEndpoint[Any, IO] = helloEndpoint.serverLogicSuccess(user => IO.pure(s"Hello ${user.name}"))
+
+  val booksListing: PublicEndpoint[Unit, Unit, List[Book], Any] = endpoint.get
+    .in("books" / "list" / "all")
+    .out(jsonBody[List[Book]])
+  val booksListingServerEndpoint: ServerEndpoint[Any, IO] = booksListing.serverLogicSuccess(_ => IO.pure(Library.books))
+
+  val apiEndpoints: List[ServerEndpoint[Any, IO]] = List(helloServerEndpoint, booksListingServerEndpoint)
+
+  val docEndpoints: List[ServerEndpoint[Any, IO]] = SwaggerInterpreter()
+    .fromServerEndpoints[IO](apiEndpoints, "swagger doc", "1.0.0")
+
+  val all: List[ServerEndpoint[Any, IO]] = apiEndpoints ++ docEndpoints
 }
