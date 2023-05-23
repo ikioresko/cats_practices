9631ce3590dcb94616d78af53a8ea6c56fb6716f
ikioresko <ikioresko@gmail.com>
2023-05-23 19:22:34 +0500

    Deferred

---
+++ src/main/scala/cats_task/concurrent/DeferredApp.scala
@@ -0,0 +1,66 @@
+package cats_task.concurrent
+
+import cats.effect._
+import cats.implicits._
+
+import scala.concurrent.duration.DurationInt
+
+object DeferredApp extends IOApp {
+  case class Item(id: Long)
+
+  def loadItems(): IO[List[Item]] = {
+        IO.raiseError(new Exception("Failed to load items"))
+//    IO.println("Loading items") *>
+//      IO.sleep(2.seconds) *>
+//      IO.println("loading done")
+//        .as(List(Item(1), Item(2)))
+  }
+
+  def initUI(): IO[Unit] = {
+    IO.println("Init UI") *>
+      IO.sleep(2.seconds) *>
+      IO.println("UI init done")
+  }
+
+  def showItems(item: List[Item]): IO[Unit] = IO.println("Showing items")
+
+  def showError(): IO[Unit] = IO.println("Showing error")
+
+  //  def setupUI(): IO[Unit] = {
+  //    initUI() *> loadItems().flatMap(items => showItems(items)).handleError(x => showError())
+  //  }
+
+  //  def setupUI(): IO[Unit] = {
+  //    (initUI(), loadItems())
+  //      .parMapN { case (_, items) =>
+  //        showItems(items)
+  //      }
+  //      .flatten
+  //      .handleError(_ => showError())
+  //  }
+  def handleUI(defItems: Deferred[IO, List[Item]]) = {
+    initUI() *> defItems.get.flatMap { items => showItems(items) }
+  }
+
+  def handleItems(defItems: Deferred[IO, List[Item]]) = {
+    loadItems().flatMap(items => defItems.complete(items)).void
+  }
+
+  def setupUI(): IO[Unit] = {
+    Deferred[IO, List[Item]].flatMap { defItems =>
+      List(handleUI(defItems), handleItems(defItems)).parSequence_
+    }
+  }
+  //  def setupUI(): IO[Unit] = {
+  //    (initUI(), loadItems().attempt)
+  //      .parMapN {
+  //        case (_, Right(items)) => showItems(items)
+  //        case (_, Left(error)) => showError()
+  //      }
+  //      .flatten
+  //  }
+
+  override def run(args: List[String]): IO[ExitCode] = {
+    setupUI().as(ExitCode.Success)
+  }
+}
