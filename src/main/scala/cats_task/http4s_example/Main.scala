469c4c52663da76a2299d9be10b893add7dbb029
ikioresko <ikioresko@gmail.com>
2023-05-27 10:03:52 +0500

    changed api

--- src/main/scala/cats_task/http4s_example/Main.scala
+++ src/main/scala/cats_task/http4s_example/Main.scala
@@ -1,18 +1,14 @@
 package cats_task.http4s_example
 
 import cats.effect._
-import cats_task.http4s_example.api.Api
+import cats_task.http4s_example.api.Api.endpoint
 import org.http4s.blaze.server.BlazeServerBuilder
-import org.http4s.server.Router
-import sttp.tapir.server.http4s.Http4sServerInterpreter
 
 object Main extends IOApp {
-  val routes = Http4sServerInterpreter[IO]().toRoutes(Api.all)
-
   def run(args: List[String]): IO[ExitCode] =
     BlazeServerBuilder[IO]
       .bindHttp(8080, "localhost")
-      .withHttpApp(Router("/" -> routes).orNotFound)
+      .withHttpApp(endpoint)
       .serve
       .compile
       .drain
